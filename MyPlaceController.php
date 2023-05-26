8659b5a83179edf4d091ad11b210da62eb1becf5
Arzhancev Michael TV-11 <104345619+Akella11@users.noreply.github.com>
2023-05-26 17:51:54 +0300

    Update MyPlaceController.php

--- MyPlaceController.php
+++ MyPlaceController.php
@@ -1,18 +1,1 @@
-<?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class MovingController extends Controller
-{
-    public function main(){
-        return view('index');
-    }
-    public function information(){
-        return view('Bear_Dream_Information');
-    }
-    public function conteiner(){
-        return view('Bear_Dream_Conteiner');
-    }
-}
