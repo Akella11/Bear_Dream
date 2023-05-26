510448d9af56e5db484b21baf0ad7583843735e3
Arzhancev Michael TV-11 <104345619+Akella11@users.noreply.github.com>
2023-05-26 17:54:07 +0300

    Update register_controller.php

--- register_controller.php
+++ register_controller.php
@@ -1,1 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class register_controller extends Controller
+{
+    public function save(Request $request){
+        
+        if(Auth::check()){
+            return redirect(route('user.private'));
+        }
+
+        $validateFields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email', 
+            'password' => 'required'
+            
+        ]);
+
+        if(User::where('email', $validateFields('email'))->exists()){
+            return redirect(route('user.registration'))->withErrors([
+                'formError' => 'Користувач з даною електронною поштою вже існує'
+            ]);    
+        }
+
+        $user = User::create($validateFields);
+        if($user){
+            Auth::login($user);
+            return redirect(route('user.private'));
+        }
+
+        return redirect(route('user.login'))->withErrors([
+            'formError' => 'Помилка при збереженні користувача'
+        ]);
+    }
+}
 
